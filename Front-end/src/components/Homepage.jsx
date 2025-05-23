77a27ac6be33ce0204f8c1650f65a2a8300ea165
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-05-23 22:16:24 +0700

    SEP490-20 adjust homepage layout

--- Front-end/src/components/Homepage.jsx
+++ Front-end/src/components/Homepage.jsx
@@ -79,6 +79,7 @@ const Homepage = () => {
             <footer className="bg-gray-800 text-white py-4 mt-8">
                 <div className="container mx-auto text-center">
                     <p>&copy; 2025 Groceries Store Management. All rights reserved.</p>
+                    
                 </div>
             </footer>
         </div>
