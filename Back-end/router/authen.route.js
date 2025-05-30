6499edd180a564142ad84a3e891d4e952f8207d7
An <annthe170622@fpt.edu.vn>
2025-05-30 21:35:38 +0700

    SEP490-13: Change code to correct file location

--- Back-end/router/authen.route.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/index');
-
-router.post('/logout', controller.authenController.logout);
