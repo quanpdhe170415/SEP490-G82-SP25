f1c7a4cc8c9f9f2aa5d84b9693a3b5b3bd1bdbf2
An <annthe170622@fpt.edu.vn>
2025-05-30 21:31:51 +0700

    SEP490-13:Create function logout in backend

--- Back-end/controller/authen.controller.js
+++ Back-end/controller/authen.controller.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcryptjs");
 // const jwt = require("jsonwebtoken");
 // const { OAuth2Client } = require("google-auth-library");
 // const nodemailer = require("nodemailer");
-const { Account} = require("../models");
+const { Account } = require("../models");
 exports.resetPassword = async (req, res) => {
   const { email, password } = req.body;
 
@@ -20,5 +20,24 @@ exports.resetPassword = async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
+  }
+};
+
+exports.logout = async (req, res) => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken) {
+    return res.status(400).json({ message: 'Refresh token không được để trống' });
+  }
+
+  try {
+    // Xoá token này khỏi DB để ngăn không cho dùng lại
+    // model RefreshToken để lưu trữ token
+    // await RefreshToken.findOneAndDelete({ refreshToken });
+
+    return res.json({ message: 'Đăng xuất thành công' });
+  } catch (err) {
+    console.error('Logout Error:', err);
+    return res.status(500).json({ message: 'Lỗi server khi đăng xuất' });
   }
 };
