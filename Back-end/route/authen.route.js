bbb58012ba58624590640e5de9e65361f9b75f36
annthe170622 <annthe170622@fpt.edu.vn>
2025-05-31 21:58:53 +0700

    Annt/sep490 13 create function logout in backend (#14)
    * SEP490-13:Create function logout in backend
    
    * SEP490-13: Change code to correct file location

--- Back-end/route/authen.route.js
+++ Back-end/route/authen.route.js
@@ -10,6 +10,8 @@ router.get("/verify-email", controller.authenController.verifyEmail);
 router.post('/reset-password', controller.authenController.resetPassword);
 router.post('/change-password', controller.authenController.changePassword);
 
+router.post('/logout', controller.authenController.logout);
+
 // các route liên quan đến otp
 router.post('/send-otp', controller.otpController.sendOTP);
 router.post('/verify-otp', controller.otpController.verifyOTP);
