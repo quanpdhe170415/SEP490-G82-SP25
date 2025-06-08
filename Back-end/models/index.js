98fc3023b1122331cfc10b79fff7af31c5d0ad69
manhbeo1709 <157514582+manhbeo1709@users.noreply.github.com>
2025-06-08 16:22:21 +0700

    update api login

--- Back-end/models/index.js
+++ Back-end/models/index.js
@@ -1,27 +1,29 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
-
 const Account = require('./account');
 const Role = require('./role');
+const UserDetail = require('./userDetail');
+const Permission = require('./permission');
 
 mongoose.Promise = global.Promise;
 
-const db = {
-  Account,
-  Role
-};
-
-db.connectDB = async () => {
-  try {
-    const mongoURI = process.env.MONGODB_URL;
-    
-    await mongoose.connect(mongoURI);
-    
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err.message);
-    process.exit(1);
-  }
-};
-
+const db = {};
+db.Account = Account;
+db.Role = Role;
+db.UserDetail = UserDetail;
+db.Permission = Permission;
 
 module.exports = db;
+
+
+// db.connectDB = async () => {
+//   try {
+//     const mongoURI = process.env.MONGODB_URL;
+    
+//     await mongoose.connect(mongoURI);
+    
+//   } catch (err) {
+//     console.error('Error connecting to MongoDB:', err.message);
+//     process.exit(1);
+//   }
+// };
+
--- Back-end/route/authen.route.js
+++ Back-end/route/authen.route.js
@@ -4,16 +4,16 @@ const controller = require('../controller/index');
 
 // các route liên quan đến authen
 router.post('/login', controller.authenController.login);
-router.post('/google-login', controller.authenController.googleLogin);
-router.post("/register", controller.authenController.register);
-router.get("/verify-email", controller.authenController.verifyEmail);
+// router.post('/google-login', controller.authenController.googleLogin);
+// router.post("/register", controller.authenController.register);
+// router.get("/verify-email", controller.authenController.verifyEmail);
 router.post('/reset-password', controller.authenController.resetPassword);
-router.post('/change-password', controller.authenController.changePassword);
+// router.post('/change-password', controller.authenController.changePassword);
 
-router.post('/logout', controller.authenController.logout);
+// router.post('/logout', controller.authenController.logout);
 
 // các route liên quan đến otp
-router.post('/send-otp', controller.otpController.sendOTP);
-router.post('/verify-otp', controller.otpController.verifyOTP);
+// router.post('/send-otp', controller.otpController.sendOTP);
+// router.post('/verify-otp', controller.otpController.verifyOTP);
 
 module.exports = router;
