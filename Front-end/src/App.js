837ab2681dc0a8a1cf5735ebac4c8534e9acd6e5
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-06-14 17:16:26 +0700

    SEP490-128-Create router for POS screen

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OpenShift from './components/OpenShift';
 import CloseShift from './components/CloseShift';
+import POS from './components/POS';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/openshift" element={<OpenShift />} />
         <Route path="/closeshift" element={<CloseShift />} />
+        <Route path="/POS" element={<POS />} />
       </Routes>
     </Router>
   );
