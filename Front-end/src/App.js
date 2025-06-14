5545cc41004eb391c55bfca1bdb2513b3270d0ff
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-06-14 16:51:05 +0700

    SEP490-121-Create router for close shift

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OpenShift from './components/OpenShift';
+import CloseShift from './components/CloseShift';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/openshift" element={<OpenShift />} />
+        <Route path="/closeshift" element={<CloseShift />} />
       </Routes>
     </Router>
   );
