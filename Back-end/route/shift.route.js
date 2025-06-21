d8aa6b93159e289c9855e28ecabb3f8ca1013a67
manhbeo1709 <157514582+manhbeo1709@users.noreply.github.com>
2025-06-21 23:28:58 +0700

    openshift route

--- Back-end/route/shift.route.js
+++ Back-end/route/shift.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controller/index');
+router.post('/handover', controller.shiftController.handoverShift);
 router.post('/openshift', controller.shiftController.openShift);
 router.post('/closeshift/:shiftId', controller.shiftController.closeShift);
 router.post('/export/:shiftId', controller.shiftController.exportShiftReport);
