37201ddef227d71415e18c05b1d3cfae93da45a3
manhbeo1709 <157514582+manhbeo1709@users.noreply.github.com>
2025-07-14 16:50:12 +0700

    UI inventory-schedule

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -16,6 +16,7 @@ import CloseShift from './components/CloseShift';
 import POS from './components/POS';
 import CashierSidebar from './components/CashierSidebar';
 import Sidebar2 from './components/Sidebar2'
+import InventorySchedule from './pages/InventorySchedule/InventorySchedule';
 
 function App() {
   return (
@@ -37,6 +38,7 @@ function App() {
         <Route path="/openshift" element={<OpenShift />} />
         <Route path="/bill-history" element={<BillHistoryPage />} />
         <Route path="/import-history" element={<ImportHistory />} />
+        <Route path="/inventory-schedule" element={<InventorySchedule />} />
 
         <Route path="/cashier/*" element={<Sidebar2 />} />
         
