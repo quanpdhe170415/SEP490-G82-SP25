61ead47ca5beb806abd7f5b19600f5ed1eee7972
annthe170622 <annthe170622@fpt.edu.vn>
2025-07-16 13:58:39 +0700

    Annt/sep490 140 fe create component sidebar and header warehouse (#66)
    * sidebar header v1
    
    * update sidebar and header warehouse v2
    
    * fix bug

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -17,8 +17,6 @@ import POS from './components/POS';
 import CashierSidebar from './components/CashierSidebar';
 
 import { UIProvider } from './contexts/UIContext';
-
-import Sidebar2 from './components/Sidebar2'
 import InventorySchedule from './pages/InventorySchedule/InventorySchedule';
 import Page from './pages/page';
 import Inventory from './pages/Inventory/Inventory';
@@ -31,7 +29,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/openshift" element={<OpenShift />} />
-
         <Route path="/closeshift" element={<CloseShift />} />
         <Route path="/POS" element={<POS />} />
         <Route path="/" element={<LoginPage />} />
@@ -47,14 +44,9 @@ function App() {
         <Route path="/openshift" element={<OpenShift />} />
         <Route path="/bill-history" element={<BillHistoryPage />} />
         <Route path="/import-history" element={<ImportHistory />} />
-
-        <Route path="/cashier/*" element={<Sidebar2 />} />
-        <Route path="/page" element={<Page />} />
-
-
         <Route path="/inventory/inventory-schedule" element={<InventorySchedule />} />
         <Route path='/inventory/inventory-schedule/inventory-control' element={<Inventory />} />
-        <Route path="/cashier/*" element={<Sidebar2 />} />
+        {/* <Route path="/cashier/*" element={<Sidebar2 />} /> */}
         <Route path="/page" element={<Page />} />
 
 
